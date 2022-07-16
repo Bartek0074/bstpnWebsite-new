92a5c39efe43844fdc917af78a3cb137d92cf6a9
Bartek0074 <88652468+Bartek0074@users.noreply.github.com>
2022-07-16 21:30:54 +0200

    created nav form and pages form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React from "react"
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Error404 from "./pages/Error404"
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Hello world!</h1>
-      <h2>I'm working on my new portofolio website!</h2>
-    </>
+    <BrowserRouter>
+          <header>
+            <nav>
+            <NavLink to={'/'}>bstpn.pl</NavLink>
+              <ul>
+                <li><NavLink to={'/about'}>About</NavLink></li>
+                <li><a target="_blank" href="https://github.com/Bartek0074">GitHub</a></li>
+                <li><NavLink to={'/contact'}>Contact</NavLink></li>
+              </ul>
+            </nav>
+          </header>
+          <section>
+            <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/about" element={<About />}/>
+              <Route path="/contact" element={<Contact />}/>
+              <Route path="*" element={<Error404 />}/>
+            </Routes>
+          </section>
+        </BrowserRouter>
   );
 }
 
